8c16cefd5ab9f7bf07c01ce19afdbd8189bbe68a
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-25 13:52:13 +0930

    finished and tested

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -6,9 +6,12 @@ using namespace std;
 
 int array_sum(int array[], int n){
 
-    
+    int sum = 0;
 
+    for (int i = 0; i < n; i = i + 1){
+        sum = sum + array[i];
+    }
 
-
+    return sum;
 
 }
