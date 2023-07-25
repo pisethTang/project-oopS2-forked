d075ee4500d59c969672321fdf6877968bb903cc
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-25 13:52:25 +0930

    finished and tested

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
 using namespace std;
 
+extern int array_sum(int[],int);
+
 int main() {
 
     //initialising
     int n = 0;
-    int array[] = {0};
 
     //finding n
     cout << "Please input the number of elements in the array: ";
     cin >> n;
-    cout << "\n";
 
-    //if n<0
+    //if n < 1
     if (n < 1) {
+
+        cout << "The sum of all elements is: 0 \n";
+
         return 0;
     }
-    else {
-        for (int i = 0; i < n; ++i){
+    else { //sum otherwise
+
+        //initialising array
+        int* array;
+        array = new int[n];
+
+        for (int i = 0; i < n; i = i + 1){
             cout << "Please input an integer for the array: ";
             cin >> array[i];
         }
 
-        for (int j = 0; j < n; ++j){
-            cout << array[j] << " ";
-        }
-    }
+        cout << "The sum of all elements is: " << array_sum(array,n) << "\n";
 
+        delete [] array;
+
+        return 0;
+
+    }
 
 }
