791c80af6eeb25fe7ba0c685ec0342f028477173
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-25 11:12:29 +0930

    created bones of function

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,1 +1,20 @@
+//this function takes an integer array and the number of elements
+//in that array, and returns the sum of all elements in the array
 
+#include <iostream>
+using namespace std;
+
+int array_sum(int array[], int n){
+
+    int n = 0;
+
+    cout << "Please input the number of elements in the array: ";
+    cin >> n;
+    cout << "\n";
+
+    cout << n;
+
+
+
+
+}
