a913e54716ec3489aef11321b3c7e1dad7ef0f24
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-25 15:40:24 +0930

    Built and tested

---
+++ function-1-3.cpp
@@ -0,0 +1,22 @@
+//this function takes in an array, the size of the array, and a number
+//and returns the amount of elements in the array that are equal
+//to the number
+
+#include <iostream>
+using namespace std;
+
+int num_count(int array[], int n, int number){
+
+    //initialise variables
+    int count = 0;
+
+    //count
+    for (int i = 0; i < n; i++){
+        if (array[i] == number){
+            count = count + 1;
+        }
+    }
+
+    return count;
+
+}
