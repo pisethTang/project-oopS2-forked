44f48adee51b4a38a8e5bde273f5045be69b74a6
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-26 14:19:37 +0930

    Built and tested somewhat

---
+++ function-3-3.cpp
@@ -0,0 +1,29 @@
+//this function takes an array and its length and returns the weighted average of that array
+
+#include <iostream>
+using namespace std;
+
+double weighted_average(int array[], int n){
+
+    //initialise variables
+    int count = 0;
+    double average;
+
+    //calculating the average
+    for (int i = 0; i < n; i++){
+        count = 0;
+
+        //counting each number
+        for (int j = 0; j < n; j++){
+            if (array[i] == array[j]){
+                count = count + 1;
+            }
+        }
+
+        //adding to the average
+        average = average + ((double(array[i]) * double(count)) / double(n));
+
+    }
+
+    return average;
+}
