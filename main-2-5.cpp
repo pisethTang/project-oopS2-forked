53329b31d02dd283e59268e4ba514daa5873762a
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-26 10:45:38 +0930

    Built and tested somewhat

---
+++ main-2-5.cpp
@@ -0,0 +1,53 @@
+//this program uses the function is_descending to determine if a given function
+//is descending or not
+
+#include <iostream>
+using namespace std;
+
+extern bool is_descending(int array[], int n);
+
+int main() {
+
+    //initialise variables
+    int n = 0;
+    bool true_or_false;
+
+     //finding n
+    cout << "How long is the array? ";
+    cin >> n;
+
+    //case of n < 1
+     if (n < 1){
+        cout << "The array is neither ascending nor descending.\n";
+        return 0;
+    }
+
+    //case of n >= 1
+    else {
+
+        //building array
+        int* array;
+        array = new int[n];
+
+        for (int i = 0; i < n; i = i + 1){
+            cout << "Please input an integer for the array: ";
+            cin >> array[i];
+        }
+
+        //using function
+        true_or_false = is_descending(array,n);
+        cout << "Is the array descending? ";
+        if (true_or_false == true){
+            cout << "Yes.\n";
+        }
+        else {
+            cout << "No.\n";
+        }
+
+        //cleaning up
+        delete array;
+
+        return 0;
+    }
+
+}
