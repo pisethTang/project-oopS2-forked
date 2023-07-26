3273ab2fa791b5beafe7f63dbb8a8bcc27d09e5a
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-26 14:48:01 +0930

    Changed for n < 1

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -8,14 +8,23 @@ int min_element(int array[], int n){
     //initialising variables
     int min_num;
 
-    //finding min_num
-    min_num = array[0];
-    for (int i = 1; i < n; i++){
-        if (array[i] < min_num){
-            min_num = array[i];
-        }
+    //checking for n < 1
+    if (n < 1) {
+        return 0;
     }
 
-    return min_num;
+    else{
+
+        //finding min_num
+        min_num = array[0];
+        for (int i = 1; i < n; i++){
+            if (array[i] < min_num){
+                min_num = array[i];
+            }
+        }
+
+        return min_num;
+        
+    }
 
 }
