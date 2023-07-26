9f31788c3bbc24cb5f91f400c9901de1811c58ac
MaddyGr3 <sevensixthree763@gmail.com>
2023-07-26 15:15:02 +0930

    wrote and tested somewhat

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,1 +1,26 @@
-//what is this on about
+//this function determines the sum of two arrays
+
+#include <iostream>
+using namespace std;
+
+int sum_two_arrays(int array[], int secondarray[], int n){
+
+    //initialise variables
+    int sum = 0;
+
+    //if n < 1
+    if (n < 1){
+        return 0;
+    }
+
+    //n >= 1
+    else {
+        //finding the sum
+        for (int i = 0; i < n; i++){
+            sum = sum + array[i] + secondarray[i];
+        }
+
+        return sum;
+    }
+
+}
