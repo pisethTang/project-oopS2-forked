611ac545df0f02cab32cdbcaafb83888043757e7
1850837 <sevensixthree763@gmail.com>
2023-08-01 05:20:46 +0930

    Finished

--- function3.cpp
+++ function3.cpp
@@ -14,8 +14,29 @@ int main() {
 
     //initialise variables
     int n = 0;
+    int j = 0;
 
-    //determine length
-    
+    //digits
+    cout << "How many digits? ";
+    cin >> n;
+
+    //make dynamic array
+    double *array = new double[n];
+
+    cout << "Enter the digits: ";
+    for (int i = 0; i < n; i++){
+        cin >> array[i];
+        j++;
+    }
+
+    //creating a non-dynamic array
+    int binary_digits[j];
+    for (int i = 0; i < n; i++){
+        binary_digits[i] = array[i];
+    }
+
+    printArray(array,n);
+
+    delete [] array;
 
 }
