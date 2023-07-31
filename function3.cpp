00bd3bd0b6cde1256cdb12accd084d4f4b664cc5
1850837 <sevensixthree763@gmail.com>
2023-08-01 05:08:14 +0930

    Built half

---
+++ function3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printArray(double* ptr, int n){
+
+    for (int i = 0; i < n; i++){
+        cout << ptr[i] << " ";
+    }
+
+    return;
+}
+
+int main() {
+
+    //initialise variables
+    int n = 0;
+
+    //determine length
+    
+
+}
