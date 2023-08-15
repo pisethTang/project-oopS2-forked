8dc57e88b960510867aea59c20d0fbe1d9708ed4
1850837 <sevensixthree763@gmail.com>
2023-08-15 12:18:27 +0930

    Debugged

--- Person.h
+++ Person.h
@@ -1,22 +1,12 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+using namespace std;
+
 //defining structure
 struct Person {
     string name;
     int age;
-    
-    //constructor
-    Person(string a, int b){
-        name = a;
-        age = b;
-    }
-
-    //standard constructor
-    Person(){
-        name = "John Doe";
-        age = 0;
-    }
 };
 
 #endif
