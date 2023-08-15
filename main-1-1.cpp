9303c8a05e7a08a14b19f06e5eb9960edc9d973e
1850837 <sevensixthree763@gmail.com>
2023-08-15 11:02:02 +0930

    Built

---
+++ main-1-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "Person.h"
+using namespace std;
+
+extern Person* createPersonArray(int n);
+
+int main() {
+
+    //initialise variables
+    int n;
+    Person* k;
+
+    //finding n
+    cout << "How many people? ";
+    cin >> n;
+
+    //using function
+    k = createPersonArray(n);
+
+    //printing
+    for (int i = 0; i < n; i++){
+        
+    }
+
+    //DELETE
+    delete k;
+
+    return 0;
+
+}
