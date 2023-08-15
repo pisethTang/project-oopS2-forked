0409599f7334fd5bdc5d207b77c8a17e9c7800b3
1850837 <sevensixthree763@gmail.com>
2023-08-15 12:17:56 +0930

    Debugged

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -9,9 +9,12 @@ Person* createPersonArray(int n) {
     //defining array of Persons
     Person* k = new Person[n];
 
+    for (int i = 0; i < n; i++){
+        k[i].name = "John Doe";
+        k[i].age = 0;
+    }
+
     //returning
     return k;
 
 }
-
-//DON'T FORGET TO DELETE
