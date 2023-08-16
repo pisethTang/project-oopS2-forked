34f41dae8334d5d49355e311aa4ae068191f70d6
1850837 <sevensixthree763@gmail.com>
2023-08-16 16:23:05 +0930

    Debugged

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -10,7 +10,17 @@ PersonList deepCopyPersonList(PersonList pl){
     PersonList p2;
 
     //finding length of p1
-    p2 = pl;
+    p2.numPeople = pl.numPeople;
+    Person* k = new Person[p2.numPeople];
+
+    for (int i = 0; i < p2.numPeople; i++){
+        k[i].name = pl.people[i].name;
+        k[i].age = pl.people[i].age;
+    }
+
+    p2.people = k;
+
+    //delete[] k;
 
     //return
     return p2;
