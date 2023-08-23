64a9dc78a7710182edc3dbe99d1b71781108b93b
1850837 <sevensixthree763@gmail.com>
2023-08-23 13:12:51 +0930

    built

---
+++ Cage.h
@@ -0,0 +1,9 @@
+
+
+Cage();                                    // a default constructor ID number is zero and occupant name is an empty string ""
+Cage(std::string newName, int newNumber);  // a constructor that takes the ID number and occupant name
+
+std::string getName();                    // returns the name of the cage occupant
+int getIDnum();                           // returns the cage's ID number
+
+~Cage();                                   // destructor 
