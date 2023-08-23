cfcf0200acda58ccacf7a6102b41817f51aebab8
1850837 <sevensixthree763@gmail.com>
2023-08-23 13:30:19 +0930

    Edited

--- Cage.cpp
+++ Cage.cpp
@@ -6,6 +6,26 @@ using namespace std;
 #include "Cage.h"
 
     //defining constructors
-    Cage(string newName, int newNumber){
+    Cage::Cage(){
+        name = "";
+        num = 0;
+    }
 
+    Cage::Cage(string newName, int newNumber){
+        name = newName;
+        num = newNumber;
+    }
+
+    //functions
+    string Cage::getName(){
+        return name;
+    }
+
+    int Cage::getIDnum(){
+        return num;
+    }
+
+    //defining destructor
+    Cage::~Cage(){
+        //why is this here??
     }
