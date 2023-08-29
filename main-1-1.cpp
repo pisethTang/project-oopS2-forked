8acc2808e084ca76b66946b5f27991c707d80795
1850837 <sevensixthree763@gmail.com>
2023-08-29 12:36:45 +0930

    Built

---
+++ main-1-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Player.h"
+#include "Wizard.h"
+#include "Warrior.h"
+using namespace std;
+int main() {
+    //Wizard(name, health, damage, mana)
+    Wizard wizard("Gandalf", 100, 20, 50);
+    //Warrior(name, health, damage, weapon)
+    Warrior warrior("Aragorn", 120, 25, "Sword");
+    cout << wizard.getName() << " has " << wizard.getHealth() << " health, and " << warrior.getName() << " has " << warrior.getHealth() << " health.\n";
+    cout << "Let the battle begin!" << endl;
+    while (wizard.getHealth() > 0 && warrior.getHealth() > 0) {
+    wizard.castSpell(&warrior);
+    if (warrior.getHealth() > 0) {
+    warrior.swingWeapon(&wizard);
+    }
+    }
+    if (wizard.getHealth() > 0) {
+    cout << wizard.getName() << " wins!" << endl;
+    } else {
+    cout << warrior.getName() << " wins!" << endl;
+    }
+    return 0;
+}
