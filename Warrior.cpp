42061c6f9fc7aadde97522bcf2ada6bb8011fad7
1850837 <sevensixthree763@gmail.com>
2023-08-29 12:36:21 +0930

    Built

---
+++ Warrior.cpp
@@ -0,0 +1,20 @@
+
+#include "Warrior.h"
+#include "Wizard.h"
+#include <iostream>
+
+Warrior::Warrior(string newName, int newHealth, int newDamage, string newWeapon):Player(newName,newHealth,newDamage){
+    setName(newName);
+    setHealth(newHealth);
+    setDamage(newDamage);
+    weapon = newWeapon;
+}
+
+//Warrior class
+void Warrior::swingWeapon(Player* opponent){
+  //  int num = rand() % 9 + 1;
+
+    cout << name << " swings their " << weapon << " at " << opponent->getName() << " for " << damage << " damage!\n";
+    
+    opponent->takeDamage(damage);
+}
