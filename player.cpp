37d24c168fc53168cab7ae71c1e5cf70178b2150
1850837 <sevensixthree763@gmail.com>
2023-08-29 12:39:13 +0930

    edited

---
+++ player.cpp
@@ -0,0 +1,56 @@
+#include "Player.h"
+#include "Warrior.h"
+#include "Wizard.h"
+#include <iostream>
+
+
+//constructors
+Player::Player(){
+    name = "";
+    health = 0;
+    damage = 0;
+}
+
+Player::Player(string newName, int newHealth, int newDamage){
+    name = newName;
+    health = newHealth;
+    damage = newDamage;
+}
+
+//functions
+void Player::setName(string newName){
+    name = newName;
+}
+
+string Player::getName(){
+    return name;
+}
+
+void Player::setHealth(int newHealth){
+    health = newHealth;
+}
+
+int Player::getHealth(){
+    return health;
+}
+
+void Player::setDamage(int newDamage){
+    damage = newDamage;
+}
+
+int Player::getDamage(){
+    return damage;
+}
+
+void Player::attack(Player* opponent, int newDamage){
+    opponent->takeDamage(newDamage);
+
+    cout << opponent->getName() << " takes " << newDamage << " damage. Remaining health: " << opponent->getHealth() << ".\n";
+
+}
+
+void Player::takeDamage(int newDamage){
+    this->health-=newDamage;
+    
+    cout << getName() << " takes " << newDamage << " damage. Remaining health: " << getHealth() << ".\n";
+}
