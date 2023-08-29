332363eeff8f6bddaaccb5d40679398918162e89
1850837 <sevensixthree763@gmail.com>
2023-08-29 12:36:26 +0930

    Built

---
+++ Warrior.h
@@ -0,0 +1,20 @@
+#ifndef WARRIOR_H
+#define WARRIOR_H
+
+#include "Player.h"
+#include <string>
+using namespace std;
+
+class Warrior : public Player{
+
+    private:
+    string weapon;
+
+    public:
+    Warrior(string newName, int newHealth, int newDamage, string newWeapon);
+    void swingWeapon(Player* opponent);
+
+};
+
+
+#endif
