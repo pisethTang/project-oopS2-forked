9f61b2cbb7f5dca7f89375908bab1391af6426bf
1850837 <sevensixthree763@gmail.com>
2023-08-31 21:39:15 +0930

    Debugged

---
+++ ParkingLot.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+#include "Vehicle.h"
+#include "ParkingLot.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+
+//constructors
+ParkingLot::ParkingLot(int newMax){
+    max = newMax;
+    count = 0;
+    vehicle = new Vehicle*[max];
+
+    for (int i = 0; i < max; i++){
+        vehicle[i] = new Vehicle(0);
+    }
+};
+
+//functions
+int ParkingLot::getCount(){
+    return count;
+}
+
+int ParkingLot::getMax(){
+    return max;
+}
+
+Vehicle** ParkingLot::getVehicle(){
+    return vehicle;
+};
+
+void ParkingLot::setVehicle(Vehicle* b, int c){
+
+    vehicle[c] = b;
+
+    return;
+}
+
+void ParkingLot::parkVehicle(Vehicle* k){
+    bool full = true;
+
+    for (int i = 0; i < getMax(); i++){
+
+        if (vehicle[i]->getID() == 0 && full == true){
+
+            setVehicle(k,i);
+
+            full = false;
+        }
+    }
+
+    if (full == true){
+        cout << "The lot is full\n";
+    }
+
+    return;
+}
+
+void ParkingLot::unparkVehicle(int ID){
+    bool match = false;
+    Vehicle c = Vehicle();
+
+    for (int i = 0; i < getMax(); i++){
+        if (ID == vehicle[i]->getID() && match == false){
+            vehicle[i] = &c;
+            match = true;
+        }
+    }
+
+    if (match == false){
+        cout << "Vehicle not in the lot\n";
+    }
+
+    return;
+}
