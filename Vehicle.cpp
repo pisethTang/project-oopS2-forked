9756233f3fe754ed3d517bf884e6f928279e19cf
1850837 <sevensixthree763@gmail.com>
2023-08-31 16:44:10 +0930

    Debugged

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -5,23 +5,15 @@ using namespace std;
 //VEHICLE_H
 //defining constructors
 Vehicle::Vehicle(){
-    //initialise variables
-    time_t timeOfEntry;
-    int ID_num;
-
     //define variables
     timeOfEntry = time(0);
-    ID_num = 0;
+    ID = 0;
 }
 
-Vehicle::Vehicle(int ID){
-    //initalise variables
-    time_t timeOfEntry;
-    int ID_num;
-
+Vehicle::Vehicle(int ID_num){
     //define variables
     timeOfEntry = time(0);
-    ID_num = ID;
+    ID = ID_num;
 }
 
 //functions
@@ -35,17 +27,17 @@ time_t Vehicle::getTimeOfEntry(){
 
 //virtual function
 int Vehicle::getParkingDuration(){ //is this function necessary?? I never call it?? how does virtual work
-    //initialise variables
-    time_t timeOfDepature;
-    int duration;
+    // //initialise variables
+    // time_t timeOfDepature;
+    // int duration;
 
-    //define variables
-    timeOfDepature = time(0);
-    duration = timeOfDepature - getTimeOfEntry();
+    // //define variables
+    // timeOfDepature = time(0);
+    // duration = timeOfDepature - getTimeOfEntry();
 
-    //test line
-    cout << timeOfDepature << " " << getTimeOfEntry() << " " << timeOfDepature - getTimeOfEntry() << "\n";
+    // //test line
+    // cout << timeOfDepature << " " << getTimeOfEntry() << " " << timeOfDepature - getTimeOfEntry() << "\n";
     cout << "Vehicle\n";
 
-    return duration;
+    return 0;
 }
