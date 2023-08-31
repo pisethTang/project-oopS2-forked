4f76e3454db0242d75552a1ae093be59fb3b2450
1850837 <sevensixthree763@gmail.com>
2023-08-31 16:44:29 +0930

    Debugged

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -17,7 +17,7 @@ int main(){
     cin >> num;
 
     //make vector and fill it
-    Vehicle* k = new Vehicle[num];
+    Vehicle** k = new Vehicle*[num];
     
     for (int i = 0; i < num; i++){
         cout << "What vehicle type? 1 for car, 2 for bus, 3 for motorbike. ";
@@ -25,13 +25,13 @@ int main(){
 
         switch(response){
             case 1:
-                k[i] = Car();
+                k[i] = new Car(1);
             break;
             case 2:
-                k[i] = Bus();
+                k[i] = new Bus(2);
             break;
             case 3:
-                k[i] = Motorbike();
+                k[i] = new Motorbike(3);
             break;
             default:
                 i = i - 1;
@@ -40,13 +40,11 @@ int main(){
 
     //use functions
     for (int i = 0; i < num; i++){
-        cout << i << ": " << k[i].getParkingDuration() <<"\n"; //this is returning insane values??
-
-        //test line
-        cout << "Testing: ";
-        k[i].getParkingDuration();
-        cout << "\n\n";
-
+        cout << i << ": " << k[i]->getParkingDuration() << "\n";
     }
+
+    delete[] k;
+
+    return 0;
 
 }
