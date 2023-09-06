1a5e2d4f21a4b35245cd5b8d460b92cbee1baa66
1850837 <sevensixthree763@gmail.com>
2023-09-06 14:32:27 +0930

    my files

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-    Helicopter a = Helicopter(10,"Sally");
+    Helicopter a = Helicopter(5690,"Sally");
 
     cout << "The weight is " << a.get_weight() << " and the fuel level is " << a.get_fuel();
     cout << " and the number of flights is " << a.get_numberOfFlights() << "\n";
@@ -14,6 +14,10 @@ int main(){
 
     cout << "The weight is " << a.get_weight() << " and the fuel level is " << a.get_fuel();
     cout << " and the number of flights is " << a.get_numberOfFlights() << "\n";
+
+    a.set_fuel(80);
+    a.fly(45,10);
+
 
     return 0;
 
