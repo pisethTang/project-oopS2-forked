ed7009daecd35d5ab043ad6925328107a7a3b8dc
1850837 <sevensixthree763@gmail.com>
2023-09-06 13:25:32 +0930

    Edited

--- AirCraft.cpp
+++ AirCraft.cpp
@@ -1,45 +1,45 @@
 #include "AirCraft.h"
 
 //constructors
-AirCraft AirCraft(){
+AirCraft::AirCraft(){
 
 }
 
-AirCraft AirCraft(int w){
+AirCraft::AirCraft(int w){
     weight = w;
 }
 
-int Aircraft::get_weight(){
+int AirCraft::get_weight(){
     return weight;
 }
-void Aircraft::set_weight(int newWeight){
+void AirCraft::set_weight(int newWeight){
     weight = newWeight;
     return;
 }
 
-float Aircraft::get_fuel(){
+float AirCraft::get_fuel(){
     return fuel;
 }
 
-void Aircraft::set_fuel(double newFuel){
+void AirCraft::set_fuel(double newFuel){
     fuel = newFuel;
     return;
 }
 
-int Aircraft::get_numberOfFlights(){
+int AirCraft::get_numberOfFlights(){
     return numberOfFlights;
 }
 
-void Aircraft::set_numberOfFlights(int newNumberOfFlights){
+void AirCraft::set_numberOfFlights(int newNumberOfFlights){
     numberOfFlights = newNumberOfFlights;
 }
 
-void Aircraft::refuel(){
+void AirCraft::refuel(){
     set_fuel(100);
     return;
 }
 
-void Aircraft::fly(int headwind, int minutes){
+void AirCraft::fly(int headwind, int minutes){
     set_numberOfFlights(get_numberOfFlights() + 1);
     return;
 }
