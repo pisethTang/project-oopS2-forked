9ad688b6d222f2cfb06b1eaee2d4b732f1732dd0
1850837 <sevensixthree763@gmail.com>
2023-10-03 11:04:52 +1030

    stored document things

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -12,6 +12,15 @@ int main(){
     // Farm f2("Farm test");
     // f2.run();
     
+    //List of problems:
+// Dependency between Land.h and Farm.h
+// Land.cpp: harvestProduce() requires access to Farm.h’s money variable
+// Land.cpp: plantProduce() requires access to Farm.h’s money variable
+// Perhaps putting planting and harvesting in Farm.h?
+// Land.cpp: plantProduce() has a line which may not work, turning a Produce into each of the 6 sub-sub-classes
+// Not using the Crops or Animals sub-classes
+// Farm.cpp: changeDay() requires being able to access Animals.h specific stuff. To alleviate this I tried to replace the Produce planted variable in Land.h with two different variables, a Crops and an Animals variable, and then have equivalents of the bool empty_or_used so that there isn’t anything going wrong, but because those two classes are pure abstract classes, they cannot exist. So unsure on how to access these specific functions?
+
 
 
     return 0;
