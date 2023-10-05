4d091acc5fba579c8f506a62ad4694e241cb466f
1850837 <sevensixthree763@gmail.com>
2023-10-05 14:16:36 +1030

    Changes to the unit testing functions

---
+++ testCarrots.h
@@ -0,0 +1,27 @@
+#ifndef CARROTSTEST_H
+#define CARROTSTEST_H
+
+
+#include <iostream>
+#include "Farm.h"
+#include "Carrots.h"
+
+class CarrotsTest {
+public:
+    void runTests() {
+        testGetCostPerDay();
+    // Add other test methods here
+    };
+
+private:
+    void testGetCostPerDay(){
+         {
+        Carrots c1;
+        if(c1.getCostPerDay() !=10){
+            std::cout << "Test 1 failed!" << std::endl;
+        }
+        }
+    }
+};
+
+#endif
