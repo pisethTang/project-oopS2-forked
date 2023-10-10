4d2f212b18abbbce4debbcd0cd11e55ba7758674
root <root@LAPTOP-JRH1S3IV>
2023-10-10 11:26:56 +1030

    removed carrot test

--- test.cpp
+++ test.cpp
@@ -1,16 +1,11 @@
 #include "FarmTest.h"
-#include "CarrotsTest.h"
+//#include "testCarrots.h"
 #include <iostream>
 
 int main() {
     // from farm test file
     FarmTest farmTest;
     farmTest.runTests();
-
-    //carrots test
-    CarrotsTest carrotsTest;
-    carrotsTest.runTests();
-
 
     return 0;
 }
