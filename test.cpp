015053452e5c8b60f1ff8db5a220e5ca8621b2a2
sophiaswita <sophia.swita@gmail.com>
2023-10-10 16:44:48 +1030

    removed carrot test

--- test.cpp
+++ test.cpp
@@ -1,16 +1,10 @@
 #include "FarmTest.h"
-#include "CarrotsTest.h"
 #include <iostream>
 
 int main() {
     // from farm test file
     FarmTest farmTest;
     farmTest.runTests();
-
-    //carrots test
-    CarrotsTest carrotsTest;
-    carrotsTest.runTests();
-
-
+    
     return 0;
 }
